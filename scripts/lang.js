bd466949dcc1f543ee34552612edf6112d9ef47a
Jonathan DURAND <djjd50@hotmail.fr>
2022-07-05 11:13:25 +0200

    Change location of language, continue style

---
+++ scripts/lang.js
@@ -0,0 +1,29 @@
+/**
+ * La structure de la table de traduction est la suivante
+ * LANGUE
+ * ↓
+ * ELEMENT
+ * ↓
+ * TEXT, SELECTOR
+ * 
+ * TODO : Gestion de cookie pour garder la langue par défaut
+ */
+
+ var dataLang = {
+    "en" : {
+        "title" : {
+            "text" : "",
+            "selector" : ""
+        }
+    },
+    "fr" : {
+        "title" : {
+            "text" : "",
+            "selector" : ""
+        }
+    },
+}
+
+function changeLang(lang) {
+    
+}
