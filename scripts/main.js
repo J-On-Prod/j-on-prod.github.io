c69f6a6b4ce9fb6637bf076540b0a1c981d57bfe
Jonathan DURAND <djjd50@hotmail.fr>
2022-07-08 19:04:12 +0200

    Add navbar script

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,45 @@
-let navElement = document.querySelector('nav');
-let presentationElement = document.querySelector('#about-me');
+window.addEventListener('scroll', reveal);
 
+function addOrRemoveReveal(reveal) {
+    const windowHeight = window.innerHeight;
+    const revealTop = reveal.getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+        reveal.classList.add('active-reveal');
+    } else {
+        reveal.classList.remove('active-reveal');
+    }
+}
+
+function showOrHideNavbar() {
+    const windowHeight = window.innerHeight;
+    const presentationTop = document.querySelector('#presentation').getBoundingClientRect().top;
+    const navbar = document.querySelector('nav');
+
+    console.log(presentationTop);
+
+    if (presentationTop < 100) {
+        navbar.style.display = 'flex';
+    } else {
+        navbar.style.display = 'none';
+    }
+}
+
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+    for (let reveal of reveals) {
+        addOrRemoveReveal(reveal);
+    }
+
+    showOrHideNavbar();
+}
+
+
+function openDetailProject(idProject) {
+
+}
+
+function closeDetailProject() {
+
+}
