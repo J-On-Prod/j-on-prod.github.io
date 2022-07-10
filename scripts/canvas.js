096c4fdda03c0665492705df381684f49297493f
Jonathan DURAND <djjd50@hotmail.fr>
2022-07-10 11:52:04 +0200

    Change all icon to SVG

---
+++ scripts/canvas.js
@@ -0,0 +1,18 @@
+function skillBackGround(listSVG, color) {
+
+}
+
+function setupProgrammingBackground() {
+    const pathSVGProg = '';
+    const nameSVGProg = [
+        'Android_robot.svg',
+        'Angular_full_color_logo.svg',
+        'Bash_Logo_Colored.svg',
+        'C_Programming_Language.svg',
+        '',
+        '',
+        '',
+        '',
+
+    ];
+}
