556dd9f56182b87b2eb2460c522a9b65ade19159
Jonathan DURAND <djjd50@hotmail.fr>
2022-07-10 12:11:30 +0200

    small changes on svg

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -7,12 +7,20 @@ function setupProgrammingBackground() {
     const nameSVGProg = [
         'Android_robot.svg',
         'Angular_full_color_logo.svg',
-        'Bash_Logo_Colored.svg',
         'C_Programming_Language.svg',
-        '',
-        '',
-        '',
-        '',
-
+        'CSS3_logo.svg',
+        'Git_icon.svg',
+        'Gnu-bash-logo.svg',
+        'Haskell-Logo.svg',
+        'HTML5_logo_and_wordmark.svg',
+        'Java_Logo.svg',
+        'Javascript_badge.svg',
+        'Node.js_logo_2015.svg',
+        'PHP-logo.svg',
+        'powershell.svg',
+        'Python-logo-notext.svg',
+        'rocket.svg',
+        'sql.svg',
+        'VB.NET_Logo.svg',
     ];
 }
